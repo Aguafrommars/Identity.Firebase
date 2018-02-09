211236cfa53858e2d2cf2a93969c93acf0170663
Olivier Lefebvre <olivier.lefebvre@live.com>
2018-02-10 00:01:47 +0100

    publish coverage result

--- cover.cmd
+++ cover.cmd
@@ -3,6 +3,6 @@ tools\dotCover\dotCover.exe cover /TargetExecutable="C:\Program Files\dotnet\dot
 
 tools\dotCover\dotCover.exe merge /Source="test\Aguacongas.Firebase.Test\coverage.snapshot;test\Aguacongas.Firebase.Test\coverage.snapshot" /Output="test\coverage.snapshot"
 tools\dotCover\dotCover.exe report /Source="test\coverage.snapshot" /Output="test\coverage.xml" /ReportType="DetailedXML"
-tools\dotCover\dotCover.exe report /Source="test\coverage.snapshot" /Output="docs\coverage.html" /ReportType="HTML"
+tools\dotCover\dotCover.exe report /Source="test\coverage.snapshot" /Output="docs\index.html" /ReportType="HTML"
 
 tools\ReportGenerator\ReportGenerator.exe "-reports:test\coverage.xml" "-targetdir:docs" "-reporttypes:Badges"
