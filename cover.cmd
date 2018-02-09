005ac3b178414797b3d03f33918087558b3ac9bd
Olivier Lefebvre <olivier.lefebvre@live.com>
2018-02-10 00:17:06 +0100

    change coverage result location

--- cover.cmd
+++ cover.cmd
@@ -1,8 +1,8 @@
-tools\dotCover\dotCover.exe cover /TargetExecutable="C:\Program Files\dotnet\dotnet.exe" /TargetArguments="test" /TargetWorkingDir="test\Aguacongas.Firebase.Test" /Output="test\Aguacongas.Firebase.Test\coverage.snapshot" /Filters="+:Aguacongas.Firebase"
-tools\dotCover\dotCover.exe cover /TargetExecutable="C:\Program Files\dotnet\dotnet.exe" /TargetArguments="test" /TargetWorkingDir="test\Aguacongas.Identity.Firebase.Test" /Output="test\Aguacongas.Identity.Firebase.Test\coverage.snapshot" /Filters="+:Aguacongas.Identity.Firebase"
+tools\dotCover\dotCover.exe cover /TargetExecutable="C:\Program Files\dotnet\dotnet.exe" /TargetArguments="test" /TargetWorkingDir="test\Aguacongas.Firebase.Test" /Output="coverage\Aguacongas.Firebase.Test.snapshot" /Filters="+:Aguacongas.Firebase"
+tools\dotCover\dotCover.exe cover /TargetExecutable="C:\Program Files\dotnet\dotnet.exe" /TargetArguments="test" /TargetWorkingDir="test\Aguacongas.Identity.Firebase.Test" /Output="coverage\Aguacongas.Identity.Firebase.Test.snapshot" /Filters="+:Aguacongas.Identity.Firebase"
 
-tools\dotCover\dotCover.exe merge /Source="test\Aguacongas.Firebase.Test\coverage.snapshot;test\Aguacongas.Firebase.Test\coverage.snapshot" /Output="test\coverage.snapshot"
-tools\dotCover\dotCover.exe report /Source="test\coverage.snapshot" /Output="test\coverage.xml" /ReportType="DetailedXML"
-tools\dotCover\dotCover.exe report /Source="test\coverage.snapshot" /Output="docs\index.html" /ReportType="HTML"
+tools\dotCover\dotCover.exe merge /Source="coverage\Aguacongas.Identity.Firebase.Test.snapshot;test\coverage\Aguacongas.Firebase.Test.snapshot" /Output="coverage\coverage.snapshot"
+tools\dotCover\dotCover.exe report /Source="coverage\coverage.snapshot" /Output="coverage\coverage.xml" /ReportType="DetailedXML"
+tools\dotCover\dotCover.exe report /Source="coverage\coverage.snapshot" /Output="docs\index.html" /ReportType="HTML"
 
-tools\ReportGenerator\ReportGenerator.exe "-reports:test\coverage.xml" "-targetdir:docs" "-reporttypes:Badges"
+tools\ReportGenerator\ReportGenerator.exe "-reports:coverage\coverage.xml" "-targetdir:docs" "-reporttypes:Badges"
