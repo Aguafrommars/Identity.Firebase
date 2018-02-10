948dc9571c2cd79063af67c04509a425e1fc71bf
Olivier Lefebvre <olivier.lefebvre@live.com>
2018-02-10 10:42:18 +0100

    move generated coverage files to coverage/docs

--- cover.cmd
+++ cover.cmd
@@ -3,6 +3,6 @@ tools\dotCover\dotCover.exe cover /TargetExecutable="C:\Program Files\dotnet\dot
 
 tools\dotCover\dotCover.exe merge /Source="coverage\Aguacongas.Identity.Firebase.Test.snapshot;coverage\Aguacongas.Firebase.Test.snapshot" /Output="coverage\coverage.snapshot"
 tools\dotCover\dotCover.exe report /Source="coverage\coverage.snapshot" /Output="coverage\coverage.xml" /ReportType="DetailedXML"
-tools\dotCover\dotCover.exe report /Source="coverage\coverage.snapshot" /Output="docs\index.html" /ReportType="HTML"
+tools\dotCover\dotCover.exe report /Source="coverage\coverage.snapshot" /Output="coverage\docs\index.html" /ReportType="HTML"
 
-tools\ReportGenerator\ReportGenerator.exe "-reports:coverage\coverage.xml" "-targetdir:docs" "-reporttypes:Badges"
+tools\ReportGenerator\ReportGenerator.exe "-reports:coverage\coverage.xml" "-targetdir:coverage\docs" "-reporttypes:Badges"
