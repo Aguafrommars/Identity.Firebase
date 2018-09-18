bf7f6e6039b00b599572041e19f93dbf16e690b4
Olivier Lefebvre <olivier.lefebvre@ne.ch>
2018-09-18 22:03:05 +0200

    fix: message filter for named http client
    #3

--- src/Aguacongas.Firebase/ServiceCollectionExtentions.cs
+++ src/Aguacongas.Firebase/ServiceCollectionExtentions.cs
@@ -108,7 +108,7 @@ namespace Microsoft.Extensions.DependencyInjection
 
                     return new FirebaseClient(httpClient, options);
                 })
-                .AddHttpClient(httpClientName)
+                .AddHttpClient(httpClientName, client => { })
                 .AddHttpMessageHandler<FirebaseAuthenticationHandler>();
 
             return services;
