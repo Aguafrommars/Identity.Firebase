11cf6ac00874207c9cec062e07aab131e41a44c1
Olivier Lefebvre <olivier.lefebvre@ne.ch>
2018-10-07 12:23:22 +0200

    ci: switch to coverlet
    generate coverage report wyth coverlet instead of dotcover

--- build.ps1
+++ build.ps1
@@ -20,24 +20,21 @@ if ($isLinux) {
 		   -Or $_.Name -like "*.Test.csproj" `
 		 } `
 	| % { 
-		$testArgs = "test " + $_.FullName
-		Write-Host "testargs" $testArgs
-		Write-Host "coveragefile" $coveragefile
-		JetBrains.dotCover.CommandLineTools\tools\dotCover.exe cover /TargetExecutable="C:\Program Files\dotnet\dotnet.exe" /TargetArguments="$testArgs" /Filters="-:*.Test;-:*.IntegrationTest;-:xunit.*;-:MSBuild;-:Moq;-:Google.*;-:Grpc.*;-:NuGet.MSBuildSdkResolver" /Output="$_.snapshot"
-    
+        &('dotnet') ('test', $_.FullName, '--logger', "trx;LogFileName=$_.trx", '-c', 'Release', '/p:CollectCoverage=true', '/p:CoverletOutputFormat=cobertura', '/p:Exclude=\"[*.Web.Views]*,[*]Ch.Ne.Sien.DotNetAccess.Data.Migrations.*\"')    
 		if ($LASTEXITCODE -ne 0) {
 			$result = $LASTEXITCODE
 		}
-
-		$merge = $merge + $_.Name + ".snapshot;"
 	  }
 
-	  Write-Host "merge " $merge
-	  JetBrains.dotCover.CommandLineTools\tools\dotCover.exe merge /Source="$merge" /Output="coverage\coverage.snapshot"
-	  JetBrains.dotCover.CommandLineTools\tools\dotCover.exe report /Source="coverage\coverage.snapshot" /Output="coverage\docs\index.html" /ReportType="HTML"
-	  JetBrains.dotCover.CommandLineTools\tools\dotCover.exe report /Source="coverage\coverage.snapshot" /Output="coverage\coverage.xml" /ReportType="DetailedXML"
-
-	  ReportGenerator\tools\ReportGenerator.exe "-reports:coverage\coverage.xml" "-targetdir:coverage\docs" "-reporttypes:Badges"
+    $merge = ""
+    gci -rec `
+    | ? { $_.Name -like "coverage.cobertura.xml" } `
+    | % { 
+        $path = $_.FullName
+        $merge = "$merge;$path"
+    }
+    Write-Host $merge
+    ReportGenerator\tools\ReportGenerator.exe "-reports:$merge" "-targetdir:coverage\docs" "-reporttypes:HtmlInline;Badges"
 }
 exit $result
   
