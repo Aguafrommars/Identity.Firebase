faf43c228efe670d063cbc265771ee0da3143b03
Olivier Lefebvre <olivier.lefebvre@ne.ch>
2018-12-12 19:42:42 +0100

    ci: exclude integration test for pr

--- build.ps1
+++ build.ps1
@@ -1,10 +1,48 @@
 $result = 0
 
-if ($isLinux) {
+$envar = Get-Childitem env: -Name 
+
+if ($envar -contains 'APPVEYOR_PULL_REQUEST_NUMBER')
+{
+	if ($isLinux) {
+		gci -rec `
+		| ? { $_.Name -like "*.IntegrationTest.csproj" `
+			-Or $_.Name -like "*.Test.csproj" `
+			} `
+		| % { 
+			cd $_.DirectoryName
+			dotnet test
+		
+			if ($LASTEXITCODE -ne 0) {
+				$result = $LASTEXITCODE
+			}
+		}
+	} else {
+		gci -rec `
+		| ? { $_.Name -like "*.IntegrationTest.csproj" `
+			-Or $_.Name -like "*.Test.csproj" `
+			} `
+		| % { 
+			&('dotnet') ('test', $_.FullName, '--logger', "trx;LogFileName=$_.trx", '-c', 'Release', '/p:CollectCoverage=true', '/p:CoverletOutputFormat=cobertura')    
+			if ($LASTEXITCODE -ne 0) {
+				$result = $LASTEXITCODE
+			}
+		}
+
+		$merge = ""
+		gci -rec `
+		| ? { $_.Name -like "coverage.cobertura.xml" } `
+		| % { 
+			$path = $_.FullName
+			$merge = "$merge;$path"
+		}
+		Write-Host $merge
+		ReportGenerator\tools\net47\ReportGenerator.exe "-reports:$merge" "-targetdir:coverage\docs" "-reporttypes:HtmlInline;Badges"
+	}
+	exit $result
+} else {
 	gci -rec `
-	| ? { $_.Name -like "*.IntegrationTest.csproj" `
-		   -Or $_.Name -like "*.Test.csproj" `
-		 } `
+	| ? { $_.Name -like "*.Test.csproj" } `
 	| % { 
 		cd $_.DirectoryName
 		dotnet test
@@ -12,28 +50,5 @@ if ($isLinux) {
 		if ($LASTEXITCODE -ne 0) {
 			$result = $LASTEXITCODE
 		}
-	}
-} else {
-	gci -rec `
-	| ? { $_.Name -like "*.IntegrationTest.csproj" `
-		   -Or $_.Name -like "*.Test.csproj" `
-		 } `
-	| % { 
-        &('dotnet') ('test', $_.FullName, '--logger', "trx;LogFileName=$_.trx", '-c', 'Release', '/p:CollectCoverage=true', '/p:CoverletOutputFormat=cobertura')    
-		if ($LASTEXITCODE -ne 0) {
-			$result = $LASTEXITCODE
-		}
-	  }
-
-    $merge = ""
-    gci -rec `
-    | ? { $_.Name -like "coverage.cobertura.xml" } `
-    | % { 
-        $path = $_.FullName
-        $merge = "$merge;$path"
-    }
-    Write-Host $merge
-    ReportGenerator\tools\net47\ReportGenerator.exe "-reports:$merge" "-targetdir:coverage\docs" "-reporttypes:HtmlInline;Badges"
+	}	
 }
-exit $result
-  
