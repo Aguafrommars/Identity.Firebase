58e7ec7859095f2b2355cc1a16b19f7a132fd468
Olivier Lefebvre <olivier.lefebvre@ne.ch>
2018-12-12 21:42:33 +0100

    ci: set build version with semantic-release

---
+++ appveyorinit.ps1
@@ -0,0 +1,18 @@
+npm i -g semantic-release @semantic-release/exec
+if (test-path ./nextversion.txt)
+{
+    Remove-Item ./nextversion.txt
+}
+semantic-release -b $env:APPVEYOR_REPO_BRANCH -d
+$nextversion = Get-Content ./nextversion.txt
+$tag = $env:GitVersion_PreReleaseTagWithDash
+if (![string]::IsNullOrEmpty($env:GitVersion_BuildMetaData))
+{
+    $builnumber = "$nextversion$tag+$env:GitVersion_BuildMetaData"
+}
+else 
+{
+    $builnumber = "$nextversion$tag"
+}
+Update-AppveyorBuild -version $builnumber
+dotnet restore
